7ece77585696ce3dd1fd6cf42e9c446fc44a3a0c
kevinl75 <kevin.letupe@gmail.com>
2024-11-21 16:41:28 +0100

    Updates

---
+++ _next/static/chunks/app/layout-980b07c48e0337a9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4217:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.t.bind(n,4839,23)),Promise.resolve().then(n.t.bind(n,347,23))},347:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[690,970,839,441,517,358],()=>s(4217)),_N_E=e.O()}]);
